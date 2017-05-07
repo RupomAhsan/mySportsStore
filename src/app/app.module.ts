e3fd2b86ecd3b55924cb19e6c26a9505fc7524a8
Rupom Ahsan <rupomahsan@live.com>
2017-05-07 22:58:10 +0800

    Added Product, Category and dynamic pagignations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { StoreModule } from "./store/store.module";
 
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule(
+  {
+    imports: [BrowserModule, StoreModule],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
+  })
 export class AppModule { }
